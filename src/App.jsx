1ab88b0af1d63af56fc10b9c5835dc46b74a33da
nitish-cybermindworks <nitish@cybermindworks.com>
2023-12-24 11:36:29 +0530

    Add react_fragment

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <React.Fragment>
+        <h1>Healthy Foods</h1>
+        <ul class="list-group">
+          <li class="list-group-item">An item</li>
+          <li class="list-group-item">A second item</li>
+          <li class="list-group-item">A third item</li>
+          <li class="list-group-item">A fourth item</li>
+          <li class="list-group-item">And a fifth one</li>
+        </ul>
+      </React.Fragment>
+    </>
+  );
+}
+
+export default App;

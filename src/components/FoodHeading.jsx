af389af718834597b71f080dd67a858e25a12f68
nitish-cybermindworks <nitish@cybermindworks.com>
2023-12-24 13:11:41 +0530

    Add css module

--- src/components/FoodHeading.jsx
+++ src/components/FoodHeading.jsx
@@ -1,5 +1,7 @@
+import css from "./FoodHeading.module.css";
+
 let FoodHeading = () => {
-  return <h1> Healthy Foods</h1>;
+  return <h1 className={css["food_haeding"]}> Healthy Foods</h1>;
 };
 
 export default FoodHeading;
--- src/components/item.jsx
+++ src/components/item.jsx
@@ -1,5 +1,11 @@
+import css from "./item.module.css";
+
 let Item = ({ foodItem }) => {
-  return <li className="list-group-item">{foodItem}</li>;
+  return (
+    <li className={`"list-group-item" ${css["kg-item"]}`}>
+      <span className={css["kg-span"]}>{foodItem}</span>
+    </li>
+  );
 };
 
 export default Item;
